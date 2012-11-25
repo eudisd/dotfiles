7116e6144efe21d49e7fb55ea485c657fef1a8ce
Eudis Duran <eudisd@gmail.com>
2012-11-25 12:06:20 -0500

    Using xmonad :)

---
+++ .xinitrc.d/90-xmonad.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+# "chmod +x ~/.xinitrc.d/90-xmonad.sh" to activate
+# this assumes you used the Haskell Platform and cabal-install to install 
+# xmonad
+USERWM=$HOME/Library/Haskell/bin/xmonad
