b19ea4db61db0080ecf26c26bf920ff0eefeb30f
Eudis Duran <eudisd@gmail.com>
2013-01-29 14:15:20 -0500

    Added mclocate

--- mac-setup.sh
+++ mac-setup.sh
@@ -3,3 +3,6 @@
 mkdir -p ~/.vim/autoload ~/.vim/bundle; \
 curl -Sso ~/.vim/autoload/pathogen.vim \
 https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+
+# Update locate
+sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.locate.plist
